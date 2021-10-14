b438ee1f97e0c04ba1c5dd82f16170e28e9a56f8
unknown <ranayna-oliveira@outlook.com>
2021-10-14 17:49:38 -0300

    commit inicial. Classe Empregado criada

--- Empregado.java
+++ Empregado.java
@@ -1,3 +1,21 @@
 public class Empregado {
+    private String nome; //encapsulado
+    private int idade;  // encapsulado
+    //camel case
+    public void setNome (String nome) { //essa variavel
+        this.nome = nome; // esse varivael recebe o valor daquela variavel. 
+    }
+
+    public String getNome () {
+        return this.nome;
+    }
+
     
+    public void setIdade (int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade () {
+        return this.idade;
+    }
 }
