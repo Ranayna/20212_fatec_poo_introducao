6a5292897b95755063acee60ca6533694d6b45d0
unknown <ranayna-oliveira@outlook.com>
2021-10-16 11:03:26 -0300

    Primeiro teste realizado com  sucesso

--- Empregado.java
+++ Empregado.java
@@ -1,45 +1,61 @@
 public class Empregado {
-    private String nome; //encapsulado
-    private int idade;  // encapsulado
-    private int tipo; //encapsulado 
+    private String nome;
+    private int idade;
+    private int tipo;
     private double salario;
     private double comissao;
     private double bonus;
 
-    public void setTipo (int tipo){
-        this.tipo = tipo; 
-    }
-
-    public int getTipo (){
-        return this.tipo; 
-    }
-
-    public double calculaSalario(){ //metodo
-        if (tipo == 1) {
-            return salario;
-        }    
-        else if (tipo == 2) {
-            return salario + salario * comissao;
+    public double calculaSalario (){
+        if (tipo == 1){
+        return salario;
         }
-        else if (tipo == 3) {
-            return salario + bonus; 
+        else if (tipo == 2){
+            //comissão varia de 0 a 1, é um percentual sobre o salário
+        return salario + salario * comissao;
         }
-        return 0;
-    }    
-    //camel case
-    public void setNome (String nome) { //essa variavel
-        this.nome = nome; // esse varivael recebe o valor daquela variavel. 
+        else if (tipo == 3){
+        return salario + bonus;
+        }
+        else{
+        return 0.;
+        }
     }
-
-    public String getNome () {
+    public String getNome (){
         return this.nome;
     }
-
-    public void setIdade (int idade) {
-        this.idade = idade;
+    public void setNome (String nome){
+        this.nome = nome;
     }
 
-    public int getIdade () {
+    public int getIdade (){
         return this.idade;
     }
-}
+    public void setIdade (int idade){
+        this.idade = idade;
+    }
+    public void setTipo (int tipo){
+        this.tipo = tipo;
+    }
+    public int tipo (){
+        return this.tipo;
+    }
+    public void setSalario (double salario){
+        this.salario = salario;
+    }
+    public double getSalario (){
+        return this.salario;
+    }
+    public void setComissao (double comissao){
+        this.comissao = comissao;
+    }
+    public double getComissao (){
+        return this.comissao;
+    }
+    public void setBonus (double bonus){
+        this.bonus = bonus;
+    }
+    public double getBonus (){
+        return this.bonus;
+    }          
+}    
